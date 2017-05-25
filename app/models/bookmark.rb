c97e955196ed9df565e064aa5af320d1ee258f84
Stephen Flowers <sflower0@chicagobooth.edu>
2017-05-25 16:10:31 -0500

    end of class

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,7 @@
 class Bookmark < ApplicationRecord
+
+validates :movie_id, :uniqueness => {:scope => :user_id, :message => "already bookmarked, shithead"}
+
   belongs_to :movie
   belongs_to :user
 end
